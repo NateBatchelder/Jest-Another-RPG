35d571e21520cdf536cf3d50b1743f6331f48081
Nathan Batchelder <natebatchelder@gmail.com>
2021-11-21 20:26:26 -0800

    player health functions and tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -36,3 +36,38 @@ test("gets inventory from player or returns false", () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+// todo = create a test to get information about the player's health
+test("gets player's health value", () => {
+  const player = new Player("Dave");
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  // console.log(player.getHealth());
+});
+
+// todo = create a test to check if the player is alive or dead
+test("checks if player is alive or not", () => {
+  const player = new Player("Dave");
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+// todo = removes health from player
+test("subtracts from player's health", () => {
+  const player = new Player("Dave");
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+  // console.log(player.health);
+
+  player.reduceHealth(99999);
+  // console.log(player.health);
+
+  expect(player.health).toBe(0);
+});
